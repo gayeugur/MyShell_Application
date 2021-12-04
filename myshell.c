2c7171298aa779ef0c6b50b523a7be3d5f4b5cd7
Gaye Uğur <78750442+gayeugur@users.noreply.github.com>
2021-12-04 16:25:45 +0300

    Update myshell.c

--- myshell.c
+++ myshell.c
@@ -1,90 +1,89 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-void command(char *temp,char *input,int index);
-void tekrar(char *temp);
-int parametreleriParcala(char *input,char *temp);
-int length(char* input);
+void command(char *input,char *inp[]);
 
 int main(int argc, char *argv[],char ** envp)
 {
-	int index=0;
-	while(1){
-	printf("myshell>>");
 	char *input;
 	input = (char*) malloc((100) * sizeof(char *));
-	 char *temp;
-  	temp = (char*) malloc((100) * sizeof(char *)); 
-    	gets(input,100,stdin);
-	printf("%s",input);
-	index=index+length(input);
-	printf("%d",index);
-	command(temp,input,index);
-	}
-    return 0;  
+	char *temp= (char*) malloc((100) * sizeof(char *));
+	char *inp[4];
+	int i=0;
+	while(1){
+		printf("myshell>>");
+		gets(input,100,stdin);
+		
+		temp =strtok(input, " ");
+ 		while (temp != NULL) {
+        	printf("%s \n", temp);
+    		inp[i]=temp;
+        	temp = strtok(NULL," ");
+        	i++;
+		inp[4]=NULL;
+    		}	
+		printf("------------------------ \n");
+		printf("inp[0]  --->%s\n",inp[0]);
+		printf("inp[1]  --->%s\n",inp[1]);
+		printf("inp[2]  --->%s\n",inp[2]);
+		
+		
+		command(input,inp);
+
+    }
 }
 
-void command(char *temp,char *input,int index){
-  
+
+void command(char *input,char *inp[]){
 	if(strcmp(input,"exit")==0){
 		printf("Cikis yapılıyor...\n");
 		exit(0);
 	}
-  	else if(strcmp(input,"bash")==0){
+	else if(strcmp(input,"bash")==0){
   		printf("Bash baslıyor...\n");
-  	}
-  	else if(strcmp(input,"tekrar")==0){
-  		printf("tekrar baslıyor...\n");
+  		 		
   		
-  		parametreleriParcala(input,temp);	
-  		if(strcmp(temp[0],"tekrar")==0){
-  			tekrar(temp);
+  	}
+  	else if(strcmp(inp[0],"tekrar")==0){
+  		printf("tekrar baslıyor...\n");
+  		if(inp[1]==NULL || inp[2]==NULL){
+  		printf("hata...\n");
   		}
-  	}else{
-  		printf("Yanlıs komut yazdınız...\n");
+  		
+  		int e,f;
+  		f = fork(); 
+            		if(f == 0){ 
+                		e = execv("tekrar",inp);
+                		perror("execv failed \n");
+            }
+            else{
+             //   wait(&e); 
+            }	
+  		
+  	}
+  	else if(strcmp(inp[0],"islem")==0){
+  	int f,e;
+  	printf("islem baslıyor...\n");
+  	
+  	f = fork(); //Fork yapıyoruz.
+            if(f == 0){ //Child process ile exec fonksiyonunu çağırıyoruz.
+                e = execv("islem",inp);
+                perror("execv failed \n");
+                
+            }
+            else{
+                wait(&e); //Child processin işi bitene kadar ana processi bekliyoruz.
+            }
+  	
+  	
+  	
+  	}
+  	
+  	else{
+  		printf("Yanlış bir komut girdiniz \n");
   	
   	}
 }
-int parametreleriParcala(char *input,char *temp){
- 	temp =strtok(input, " ");
- 	while (temp != NULL) {
-        	printf("%s", temp);
-        	temp = strtok(NULL, " ");
-    }
-	return 0;
-}
-
-
-
-int length(char* input){
-	int i = 0;
-    	while(input[i] != '\0'){
-     		i++;
-     	}
-    	return i;
-}
-
-void tekrar(char *temp){
-	int f,e,status;
-	f=fork();
-	printf("a");
-	if(temp[0] || temp[1]){
-		printf("hatalı giriş yapıldı");
-	}else{
-	printf("b");
-		if(f==0){
-			e=execve("tekrar",temp,NULL);
-		}else if(f<0){
-			printf("error");
-		}else{
-			status = wait(&status);
-		}
-	}
-
-}
-
-
 
 
 
