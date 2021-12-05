ac98e7d550a605dfbe1b78bb5831fafe72ea2186
Gaye Uğur <78750442+gayeugur@users.noreply.github.com>
2021-12-05 17:23:37 +0300

    Update myshell.c

--- myshell.c
+++ myshell.c
@@ -2,13 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 void command(char *input,char *inp[]);
-
+int len(char **input);
 int main(int argc, char *argv[],char ** envp)
 {
 	char *input;
 	input = (char*) malloc((100) * sizeof(char *));
 	char *temp= (char*) malloc((100) * sizeof(char *));
 	char *inp[4];
+	char *devide=(char*) malloc((100) * sizeof(char *));;
 	int i=0;
 	while(1){
 		printf("myshell>>");
@@ -21,18 +22,52 @@ int main(int argc, char *argv[],char ** envp)
         	temp = strtok(NULL," ");
         	i++;
 		inp[4]=NULL;
-    		}	
+    		}
+    		/*	
 		printf("------------------------ \n");
 		printf("inp[0]  --->%s\n",inp[0]);
 		printf("inp[1]  --->%s\n",inp[1]);
 		printf("inp[2]  --->%s\n",inp[2]);
+		printf("inp[3]  --->%s\n",inp[3]);
+		printf("inp[4]  --->%s\n",inp[4]);
 		
+		if(len(input)>4){
+		int count=0;
+		while(strcmp(inp[count],"|")==-1){
+			printf("inp[&d]  --->%s\n",count,inp[count]);
+		}
+		}	
+    		printf("------------------------ \n");
+		printf("devide[0]  --->%s\n",devide[0]);
+		printf("devide[1]  --->%s\n",devide[1]);
+		printf("devide[2]  --->%s\n",devide[2]);
+		printf("devide[3]  --->%s\n",devide[3]);
+		printf("devide[4]  --->%s\n",devide[4]);
+		
+		
+		printf("| var");
+			for(int j=0; j<len(inp); j++){
+			printf("%d \n",j);
+				printf("inp[%d]  --->---%s---\n",j,inp[j]);
+				if(strcmp(inp[j],"|")==0){
+				printf("| bulundu");
+				}
+			}
+		}	*/
+	
 		
 		command(input,inp);
 
-    }
+    
 }
-
+}
+int len(char **input){
+    int size = 0;
+    while(input[size]!=NULL){
+        size++;
+    }
+    return size+1;
+}
 
 void command(char *input,char *inp[]){
 	if(strcmp(input,"exit")==0){
@@ -41,15 +76,18 @@ void command(char *input,char *inp[]){
 	}
 	else if(strcmp(input,"bash")==0){
   		printf("Bash baslıyor...\n");
-  		 		
+  		system("/bin/bash");
+  					 		
   		
+  	}
+  	else if(strcmp(inp[0],"cat")==0){
+  		printf("cat:%s\n",inp[1]);
   	}
   	else if(strcmp(inp[0],"tekrar")==0){
   		printf("tekrar baslıyor...\n");
-  		if(inp[1]==NULL || inp[2]==NULL){
-  		printf("hata...\n");
-  		}
-  		
+  		/*if(inp[1]==NULL || inp[2]==NULL){
+  			printf("hata\n");
+  		}*/
   		int e,f;
   		f = fork(); 
             		if(f == 0){ 
@@ -57,7 +95,7 @@ void command(char *input,char *inp[]){
                 		perror("execv failed \n");
             }
             else{
-             //   wait(&e); 
+                wait(&e); 
             }	
   		
   	}
@@ -65,30 +103,33 @@ void command(char *input,char *inp[]){
   	int f,e;
   	printf("islem baslıyor...\n");
   	
-  	f = fork(); //Fork yapıyoruz.
-            if(f == 0){ //Child process ile exec fonksiyonunu çağırıyoruz.
+  	f = fork(); 
+            if(f == 0){ 
                 e = execv("islem",inp);
                 perror("execv failed \n");
                 
             }
             else{
-                wait(&e); //Child processin işi bitene kadar ana processi bekliyoruz.
+                wait(&e); 
             }
   	
   	
   	
   	}
+  	else if(strcmp(inp[0],"ls")==0){
+        
+    	   system("/bin/ls");
+        
+    }
+  	else if (strcmp(inp[0],"clear")==0){ 
+        system("clear");
+    }
   	
   	else{
   		printf("Yanlış bir komut girdiniz \n");
   	
   	}
 }
-
-
-
-
-
 
 
 
