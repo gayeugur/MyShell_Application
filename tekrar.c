7466db73f8bf560b6b8d91eabb08bf1525d85ee0
Gaye Uğur <78750442+gayeugur@users.noreply.github.com>
2021-12-11 20:52:35 +0300

    Update tekrar.c

--- tekrar.c
+++ tekrar.c
@@ -3,11 +3,10 @@
 #include <string.h>
 
 
-int main(int argc, char *inp[])
-{
-    int num = atoi(inp[2]);
-    for(int i=0; i<num; i++){
-        printf("%s \n",inp[1]);
+int main(int argc, char *inp[]){
+    int num = atoi(inp[2]); //Kullanıcıdan aldığım adet parametresini int çevirerek num değişkenine atadım.
+    for(int i=0; i<num; i++){ //Num adeti kadar dönerek kaç kez yazdırılması isteniyorsa printf ile inp[1]
+        printf("%s \n",inp[1]); //olan değeri yazdırdım (tekrar fatih 3 durumunda yazdırılmak istenen değer fatih)
     }
     return 0;
 }
