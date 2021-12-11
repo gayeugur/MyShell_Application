974db6baa333ed087c31f4acf63e1231027749ca
Gaye Uğur <78750442+gayeugur@users.noreply.github.com>
2021-12-11 20:53:26 +0300

    Update topla.c

--- topla.c
+++ topla.c
@@ -3,9 +3,9 @@
 
 int main(int argc, char *argv[])
 {
-    int num1 = atoi(argv[0]);
-    int num2 = atoi(argv[1]);
-    int total = num1+num2;
-    printf("%d + %d = %d \n",num1,num2,total);
+    int num1 = atoi(argv[0]);//Kullanıcıdan alınan değeri int çevirerek sayı değerinin birincisini num1 e atadım.
+    int num2 = atoi(argv[1]);//Kullanıcıdan alınan değeri int çevirerek sayı değerinin ikincisini num2 ye atadım.
+    int sum = num1+num2;//Aldığım sayıların toplamını sum değişkenine atadım.
+    printf("%d + %d = %d \n",num1,num2,sum);//Yazdırma işlemini gerçekleştirdim.
     return 0;
 }
