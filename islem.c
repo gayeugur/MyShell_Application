1a8bd69afe54b03c9c335ee1e30686603eed8a7b
Gaye Uğur <78750442+gayeugur@users.noreply.github.com>
2021-12-12 15:21:23 +0300

    Update islem.c

--- islem.c
+++ islem.c
@@ -2,12 +2,20 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]){
-    int e;
-    char *temp[3]; 
-    temp[0] = argv[2]; //Kullanıcıdan aldığım sayı değerlerinin birincisini atadım.
-    temp[1] = argv[3]; //Kullanıcıdan aldığım sayı değerlerinin ikincisini atadım.
-    temp[2] = NULL; //exec hatası almamak için son elemanı NULL olarak tanımladım.
+    char *temp[4]; 
+    temp[0] = argv[1]; //Kullanıcıdan aldığım topla/cikar değerini atadım.
+    temp[1] = argv[2]; //Kullanıcıdan aldığım sayı değerlerinin birincisini atadım.
+    temp[2] = argv[3]; //Kullanıcıdan aldığım sayı değerlerinin ikincisini atadım.
+    temp[3]=NULL;//exec hatası almamak için son elemanı NULL olarak tanımladım.
     
-    e = execv(argv[1],temp);//Proses içinde topla.c veya cıkar.c yi çalıştırmak için exec kullandım 
-    return 0;            //aldığım parametre topla veya cıkar olmasına göre topla.c yi ya da cıkar.c yi çalıştırdım
+    int f,e;
+    f=fork();
+    if(f==0){
+    	e = execv(argv[0],temp);//Proses içinde topla.c veya cıkar.c yi çalıştırmak için exec kullandım 
+    }else{
+    	wait(&e); //Child prosesin tamamlanmasını bekledim.
+    }
+
+    
+    return 0;            
 } 
